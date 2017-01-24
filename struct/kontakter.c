975c92942d0d7222a6265b4e3821da20690e33a9
Johan Kämpe <kampe.johan@gmail.com>
2017-01-24 10:48:52 +0100

    Removed non typedef struct

--- struct/kontakter.c
+++ struct/kontakter.c
@@ -2,19 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct person1{
-  char * namn;
-  int alder;
-}p1, p2;
-
 typedef struct _person_S{
   char * namn;
   int alder;
 }person;
 
+person p1, p2;
+
 int main(void){
   printf("Välkommen till kontakter!\n");
-  p1.namn = "Sven Specimen1";
+  p1.namn = "Sven Specimen";
   p1.alder = 32;
   p2 = p1;
   printf("person - namn: %s, ålder: %d\n",
@@ -22,13 +19,9 @@ int main(void){
   printf("person - namn: %s, ålder: %d\n",
     p2.namn, p2.alder);
 
-  /*   Pointer to non-typedef struct  */
-  struct person1 *p, kontakter[1000];
-  p = (struct person1 *)malloc(sizeof(struct person1));
-
   /*   Pointer to typedef struct */
-  person *pt, tkontakter[1000];
-  pt = (person *)malloc(sizeof(person));
+  person *p, tkontakter[1000];
+  p = (person *)malloc(sizeof(person));
 
   return 0;
 
