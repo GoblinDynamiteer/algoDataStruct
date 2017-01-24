d4ee92da7d2aaafeae83474bbfd776fa302a35d2
Johan Kämpe <kampe.johan@gmail.com>
2017-01-24 20:05:56 +0100

    Wrote aritmethic functions for fraction numbers

--- assignments/ass01/frac.c
+++ assignments/ass01/frac.c
@@ -1,5 +1,47 @@
+/*    Inlämningsuppgift 1
+      Algoritmer och datastrukturer
+
+      Johan Kämpe */
+
+
 #include "frac.h"
 
-int main(void){
+frac plus(frac f1, frac f2){
+  frac result;
+  if(f1.n == f2.n){
+    result.t = f1.t + f2.t;
+    result.n = f1.n;
+  }
+  else{
+    result.t = f1.t * f2.n + f2.t * f1.n;
+    result.n = f1.n * f2.n;
+  }
+  return result;
+}
 
+frac minus(frac f1, frac f2){
+  frac result;
+  if(f1.n == f2.n){
+    result.t = f1.t - f2.t;
+    result.n = f1.n;
+  }
+  else{
+    result.t = f1.t * f2.n - f2.t * f1.n;
+    result.n = f1.n * f2.n;
+  }
+  return result;
+}
+
+frac times(frac f1, frac f2){
+  frac result;
+  result.t = f1.t * f2.t;
+  result.n = f1.n * f2.n;
+  return result;
+}
+
+frac div(frac f1, frac f2){
+  frac result;
+  result.t = f1.t * f2.n;
+  result.n = f1.n * f2.t;
+  return result;
 }
