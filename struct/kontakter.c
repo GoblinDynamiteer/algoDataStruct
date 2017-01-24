73e341494044736f2db2132eec78420553829c90
Johan Kämpe <kampe.johan@gmail.com>
2017-01-24 11:20:36 +0100

    Input of persons

--- struct/kontakter.c
+++ struct/kontakter.c
@@ -7,26 +7,33 @@ typedef struct _person_S{
   int alder;
 }person;
 
-person p1, p2;
+person p1, p2, kontakt[10];
 
 typedef int * ptr_to_int;
 
 int main(void){
-  int pek = 2;
-  ptr_to_int a;
-  printf("Välkommen till kontakter!\n");
-  p1.namn = "Sven Specimen";
-  p1.alder = 32;
-  p2 = p1;
-  a = &pek;
-  printf("person - namn: %s, ålder: %d\n",
-    p1.namn, p1.alder);
-  printf("person - namn: %s, ålder: %d\n",
-    p2.namn, p2.alder);
+  int ix;
+  char namn[64];
 
-  /*   Pointer to typedef struct */
-  person *p, tkontakter[1000];
-  p = (person *)malloc(sizeof(person));
+  printf("Välkommen till kontakter!\n");
+
+  /*    Inmatning av kontakter till struct */
+  for(int ix = 0; ix < 3; ix++){
+      printf("Mata in kontakt nr %d \n", ix+1);
+      printf("Ange namn: "); fflush(stdin);
+      fgets(namn, 64, stdin);
+      kontakt[ix].namn = namn; //fel - pekare
+      printf("Ange ålder: "); fflush(stdout);
+      fscanf(stdin, "%d", &kontakt[ix].alder); fflush(stdin);
+    }
+
+  for(int i = 0; i< 3; i++){
+      printf("namn %d: %s\nÅlder: %d\n",
+      i,
+      kontakt[i].namn,
+      kontakt[i].alder
+    );
+  }
 
   return 0;
 
