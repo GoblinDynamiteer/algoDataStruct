0a2b3c974f53292389072147faf82525133e8748
Johan Kämpe <kampe.johan@gmail.com>
2017-01-24 20:05:29 +0100

    Comments

--- assignments/ass01/frac.h
+++ assignments/ass01/frac.h
@@ -1,11 +1,17 @@
+/*    Inlämningsuppgift 1
+      Algoritmer och datastrukturer
+
+      Johan Kämpe */
+
 
 /*	 Include guard	*/
 #ifndef FRAC_H_
 #define FRAC_H_
 
+/*    Struct för bråktal i 3/5 form */
 typedef struct _q_S{
-  int t;
-  int n;
+  int t;  //Täljare
+  int n;  //Nämnare
 }frac;
 
 frac plus(frac f1, frac f2);
