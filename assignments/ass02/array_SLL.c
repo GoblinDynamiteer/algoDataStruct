74762734f067883ed62cd66093b7cf4a3fa2a550
Johan Kämpe <kampe.johan@gmail.com>
2017-01-31 18:14:24 +0100

    Added original code for assignment 2

---
+++ assignments/ass02/array_SLL.c
@@ -0,0 +1,12 @@
+#include "array_SLL.h"
+
+array_SLL *create_SLL( int size ) {
+}
+int destroy_SLL( array_SLL *array ) {
+}
+int insert_SLL( array_SLL *array, double value ) {
+}
+double lookup_SLL( array_SLL *array, int index ) {
+}
+int find_SLL( array_SLL *array, double value ) {
+}
