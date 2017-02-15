9670d359b1b33b9edeebc69a33416bcbb9fd8eae
Johan Kämpe <kampe.johan@gmail.com>
2017-02-15 11:58:00 +0100

    Binary Trees

---
+++ binTrees/tree.c
@@ -0,0 +1,39 @@
+#include "tree.h"
+#include <stdio.h>
+
+// Om noden är "tom" returnera 1, annars 0:
+int is_empty(node *N) {
+    return (N == 0);
+}
+// Returnera innehållet i noden:
+value_type get_value(node *N) {
+    if(is_empty(N)){
+        printf("Fel! Can't get next from empty!");
+    }
+    else{
+        return N->val;
+    }
+}
+// Returnera barnlänk till vänster, eller höger:
+node *get_left(node *N) {
+    if(is_empty(N)){
+        printf("Fel! Can't get next from empty!");
+    }
+    else{
+        return N->left_child;
+    }
+}
+node *get_right(node *N) {
+    if(is_empty(N)){
+        printf("Fel! Can't get next from empty!");
+    }
+    else{
+        return N->right_child;
+    }
+}
+// Sätt in ett värde i trädet:
+node *insert_value(value_type val, node *Tree) {
+}
+// Sök efter ett värde i trädet:
+node *find_value(value_type val, node *Tree) {
+}
---
+++ binTrees/tree.h
@@ -0,0 +1,18 @@
+typedef int value_type;
+
+typedef struct _node_S{
+  value_type val;
+  struct _node_S * left_child, * right_child;
+}node;
+
+// Om noden är "tom" returnera 1, annars 0:
+int is_empty(node *N);
+// Returnera innehållet i noden:
+value_type get_value(node *N);
+// Returnera barnlänk till vänster, eller höger:
+node * get_left(node *N);
+node * get_right(node *N);
+// Sätt in ett värde i trädet:
+node * insert_value(value_type val, node *Tree);
+// Sök efter ett värde i trädet:
+node * find_value(value_type val, node *Tree);
