fe48ee9c7429d89ad96b1952e999d4414204ae1e
Johan Kämpe <kampe.johan@gmail.com>
2017-02-16 09:53:53 +0100

    Fixed value return functions

--- binTrees/tree.c
+++ binTrees/tree.c
@@ -1,5 +1,6 @@
 #include "tree.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 // Om noden är "tom" returnera 1, annars 0:
 int is_empty(node *N) {
@@ -10,29 +11,33 @@ value_type get_value(node *N) {
     if(is_empty(N)){
         printf("Fel! Can't get next from empty!");
     }
-    else{
-        return N->value;
-    }
+    return N->value;
 }
 // Returnera barnlänk till vänster, eller höger:
 node *get_left(node *N) {
     if(is_empty(N)){
         printf("Fel! Can't get next from empty!");
     }
-    else{
-        return N->left_child;
-    }
+    return N->left_child;
 }
 node *get_right(node *N) {
     if(is_empty(N)){
         printf("Fel! Can't get next from empty!");
     }
-    else{
-        return N->right_child;
-    }
+    return N->right_child;
 }
 // Sätt in ett värde i trädet:
-node *insert_value(value_type val, node *Tree) {
+node *insert_value(value_type val, node *Tree){
+    if(is_empty(Tree)){
+        node * new_node = (node *)malloc(sizeof(node));
+        new_node->value = val;
+        new_node->right_child = NULL;
+        new_node->left_child = NULL;
+        return new_node;
+    }
+    else{
+
+    }
 }
 // Sök efter ett värde i trädet:
 node *find_value(value_type val, node *Tree) {
